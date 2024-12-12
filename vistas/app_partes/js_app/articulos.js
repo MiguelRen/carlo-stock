16392c4777fbafa611c0ec87f11cfbbe92787c87
Miguel Rengel <miguelr@vitalclinic.local>
2024-12-12 17:42:42 -0400

    adding new cells on modal table

--- vistas/app_partes/js_app/articulos.js
+++ vistas/app_partes/js_app/articulos.js
@@ -9,57 +9,62 @@ const modal_button = document.getElementById("boton_agregar_art");
 modal_button.addEventListener('click',(e) => {
   
   e.preventDefault();
+
   modal1.classList.add('modal1-show');
+
+  
+  agregarFila()
+
 });
 
 
 //trayendo el boton de agregar nuevo renglon en el modal
 const ag_det_button = document.getElementById("ag_det_button");
 
+let contador_fila = 0;
 
 //trayendo la tabla entera del modal
-const tabla = document.getElementById("tabla_modal");
+// const tabla = document.getElementById("tabla_modal");
 
 
+function agregarFila(){
+  // console.log(tabla);
+  
+
+  let nueva_fila = document.createElement('tr');
+// console.log(nueva_fila);
+
+nueva_fila.innerHTML =`
+  <td  id="modal-descripcion_${contador_fila}">
+    <input class="w" type="text" class="w" >
+  </td>
+  <td  id="modal-unidad_${contador_fila}">
+    <input class="w" type="text" >
+  </td>
+  <td  id="modal-tipo_art_${contador_fila}">
+    <input class="w" type="text" >
+  </td>
+  <td  id="modal-cantidad_${contador_fila}">
+    <input class="w" type="text" >
+  </td>
+  <td  id="modal-precio_${contador_fila}">
+    <input class="w" type="text" >
+  </td>
+  <td  id="modal-iva_${contador_fila}">
+    <input class="w" type="text" >
+  </td>
+  <td  id="modal-neto_${contador_fila}">
+    <input class="w" type="text" >
+  </td>
+  `;
+  
+  document.getElementById('tbody_modal').appendChild(nueva_fila);
+  contador_fila = contador_fila + 1;
+}
 ag_det_button.addEventListener('click', e => {
    
 
-  function agregarFila(tabla){
-    console.log(tabla);
-    
-    let contador_fila = 0;
 
-    let nueva_fila = document.createElement('tr');
-// console.log(nueva_fila);
-
-  nueva_fila.innerHTML =`
-    <td id="modal-descripcion_${contador_fila}">
-      <input type="text" class="w" >
-    </td>
-    <td id="modal-unidad_${contador_fila}">
-      <input type="text" class="w" >
-    </td>
-    <td id="modal-tipo_art_${contador_fila}">
-      <input type="text" class="w" >
-    </td>
-    <td id="modal-cantidad_${contador_fila}">
-      <input type="text" class="w" >
-    </td>
-    <td id="modal-precio_${contador_fila}">
-      <input type="text" class="w" >
-    </td>
-    <td class="w" id="modal-iva_${contador_fila}">
-      <input type="text" >
-    </td>
-    <td id="modal-neto_${contador_fila}">
-      <input type="text" class="w" >
-    </td>
-    `;
-    
-    document.getElementById('tbody_modal').appendChild(nueva_fila);
-    contador_fila ++;
-  }
-
-  agregarFila(tabla);
+  agregarFila();
 
 });
--- vistas/app_partes/modales/articulos.php
+++ vistas/app_partes/modales/articulos.php
@@ -1,8 +1,8 @@
 <section class="modal1 d-flex">
     <div class="modal_container ">
-        
-        
-        
+
+
+
         <a href="" class="close">Cerrar</a>
         <div class="exterior_tabla">
 
@@ -17,53 +17,34 @@
                         <tr>
                             <th>Descripcion</th>
                             <th>Unidad</th>
+                            <th>Tipo Articulo</th>
                             <th>Cantidad</th>
                             <th>Precio Unitario</th>
                             <th>IVA</th>
+                            <th>Precio Neto</th>
                         </tr>
                     </thead>
-                    <tbody class="w"  id="tbody_modal">
-                        <tr>
-                            <td>
-                                <input class="w" type="text" name="modal-descripcion" value="">
-                            </td>
-                            <td>
-                                <input class="w" type="text" name="modal-unidad">
-                            </td>
-                            <td>
-                                <input class="w" type="text" name=" modal-tipo_art">
-                            </td>
-                            <td>
-                                <input class="w" type="text" name="modal-cantidad">
-                            </td>
+                    <tbody class="" id="tbody_modal">
 
+                    </tbody>
+                    <tfoot>
 
-                            <td>
-                                <input class="w" type="text" name="modal-precio">
-                            </td>
-                            <td>
-                                <input class="w" type="text" name="modal-iva">
-                            </td>
-                            <td>
-                                <input class="w" type="text" name="modal-neto">
-                            </td>
-                        </tr>
                         <tr>
                             <td><button name accion="accion" value="ag_detalle" class="btn btn-primary"
                                     id="ag_det_button">+</button></td>
                         </tr>
-                    </tbody>
-                   
+                    </tfoot>
+
                 </table>
 
             </div>
 
 
-           
-                                <button type="submit" name="accion" value="Agregar" class="btn btn-primary" >Guardar y
-                                    Finalizar</button>
-                                <button>Cancelar</button>
-                        
+
+            <button type="submit" name="accion" value="Agregar" class="btn btn-primary">Guardar y
+                Finalizar</button>
+            <button>Cancelar</button>
+
         </div>
 
 
